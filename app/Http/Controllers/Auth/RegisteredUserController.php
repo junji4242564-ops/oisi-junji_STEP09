56fcfbbda6af8860256a65d96754b41d70511bdf
junji <junji4242564@gmail.com>
2026-09-14 16:53:23 +0900

    api修正

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -43,10 +43,8 @@ class RegisteredUserController extends Controller
            'password' => Hash::make($validated['password']),
         ]);
 
-        event(new Registered($user));
+           event(new Registered($user));
 
-        Auth::login($user);
-
-        return redirect(route('dashboard', absolute: false));
+        return redirect()->route('login');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-Route::post('/products/{product}/purchase', [SaleController::class, 'store']);
-Route::middleware('auth:sanctum')->post('/products/{id}/purchase', [ProductController::class, 'purchase']);
+    Route::post('/products/{product}/purchase', [SaleController::class, 'store']);
 });
